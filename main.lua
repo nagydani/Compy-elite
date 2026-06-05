5a482da77457c61f6eb8e365390b876e93a472b0
Daniel A. Nagy <nagy.da@gmail.com>
2026-06-05 04:57:25 +0200

    Do not rely on `gfx.reset()`

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ SCALE = 4
 gfx = love.graphics
 gfx.reset()
 gfx.clear(Color[0])
+gfx.setColor(Color[15])
+gfx.setFont(gfx.newFont(10))
 for _, star in ipairs(galaxy(23114, 584, 46931)) do
   gfx.print(star.name, star.x * SCALE, star.y * SCALE)
 end
